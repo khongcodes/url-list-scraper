9a9e3ec5a82591820c7414250f9f284fecbfb7fd
KHong <albtrossfacefall@gmail.com>
2024-02-14 10:11:11 -0800

    import from CSV and work off of a vector

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,66 @@
-// use std::{collections::HashMap, hash::Hash};
-
-// use hyper::client;
-// use std::io::Read;
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let resp = reqwest::blocking::get("https://httpbin.org/ip")?
-//     .json::<HashMap<String, String>>();
-    
-//     println!("{:#?}", resp);
-//     Ok(())
-// }
-
-// fn main() {
-//     let client = Client::new();
-//     let mut res = client.get("http://www.bloomberg.com/")
-//         .send()
-//         .unwrap();
-//     let mut body = String::new();
-//     res.read_to_string(&mut body).expect("failed to read into string");
-//     println!("{}", body);
-// }
-
-// use hyper::http::Method;
 use reqwest::{ Url, Request, Method, Client };
-use url::ParseError;
+use regex::Regex;
+use std::error::Error;
 
 #[cfg(not(target_arch="wasm32"))]
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
 
-    
-    let url = Url::parse("https://hyper.rs").expect("URL ParseError");
-
-    eprintln!("Fetching {url:?}...");
-
-    let request: Request = Request::new(Method::GET, url);
+    let user_agent = if let Some(user_agent) = std::env::args().nth(1) {
+        user_agent
+    } else {
+        println!("No user agent provided: using default");
+        String::from("user")
+    };
 
     let client: Client = Client::builder()
-        .user_agent("kevin")
+        .user_agent(user_agent)
         .https_only(true)
         .build()?;
 
+    // IMPORT CSV
+    let url_list = if let Ok(vec) = get_list_from_csv() {
+        vec
+    } else {
+        panic!()
+    };
 
-    let res = reqwest::RequestBuilder::from_parts(client, request)
-        .send()
-        .await?;
+    for url in url_list {
+        let url_formatted = Url::parse(&url).expect("URL parse error from vec");
 
-    eprintln!("Response: {:?} {}", res.version(), res.status());
-    eprintln!("Headers: {:#?}\n", res.headers());
+        let request: Request = Request::new(Method::GET, url_formatted);
 
-    let body = res.text().await?;
-    
-    println!("{:?}", body.contains("<iframe"));
+        eprintln!("Fetching {url:?}...");
+
+        let res = reqwest::RequestBuilder::from_parts(client.clone(), request)
+            .send()
+            .await?;
+
+            eprintln!("Response: {:?} {}", res.version(), res.status());
+        eprintln!("Headers: {:#?}\n", res.headers());
+
+        let body = res.text().await?;
+
+        // This regex expression searches for iframe code sourced to https://www.youtube.com
+        let re = Regex::new(r#"<iframe.*src="https:\/\/www\.youtube\.com.*\".*>"#).unwrap();
+
+        println!("{:?}", re.is_match(&body));
+
+    }
     
     Ok(())
+}
+
+fn get_list_from_csv() -> Result<Vec<String>, Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path("urls.csv")?;
+    let mut results: Vec<String> = vec![];
+
+    for result in rdr.records() {
+        let record = result?;
+        results.push(String::from(record.get(0).unwrap()));
+    }
+
+    println!("{:?}", results);
+
+    Ok(results)
 }
